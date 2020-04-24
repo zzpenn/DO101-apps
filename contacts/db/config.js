49602f248098833112ec01235b25a3daf676ce90
zzpenn <zzpenn@gmail.com>
2020-04-24 14:59:09 -0700

    hard code DB_CONFIG -  no secret

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@contactsdb:5432/contacts';
+//const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@contactsdb:5432/contacts';
+const DB_CONFIG = 'postgresql://contacts:contacts@contactsdb:5432/contactsdb';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
